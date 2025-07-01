9571eb858d3844449c62054e0ecf9ebd1bb09182
Markus Huckriede <markushuckriede@MacBook-Air-von-Markus.local>
2025-07-01 15:09:55 +0200

    json replaced with API search

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,4 +1,4 @@
-import json
+import requests
 
 with open("animals_template.html", "r") as file:
     template = file.read()
@@ -19,17 +19,37 @@ def serialize_animal(animal_obj):
     output += '</li>'
     return output
 
-def main():
-    animals_data = load_data("animals_data.json")
 
-    animals_info = ''
-    for animal_obj in animals_data:
-        animals_info += serialize_animal(animal_obj)
+def fetch_animal_data(name):
+    api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(name)
+    response = requests.get(api_url, headers={'X-Api-Key': '0K1fwHLpvK0kCtztNfwR7Q==xpgs5EGeVJ76ua69'})
+    if response.status_code == requests.codes.ok:
+        data = response.json()
+        if data:
+            return data
+        else:
+            print(f"The animal {name} doesn't exist")
+            return None
+    else:
+        print("Error:", response.status_code, response.text)
+        return None
+
+def main():
+    name = input("Enter a name of an animal: ")
+    animals_data = fetch_animal_data(name)
+
+    if not animals_data:
+        animals_info = f" <h2>The animal '{name}' doesn't exist.</h2>"
+    else:
+        animals_info = ''
+        for animal_obj in animals_data:
+            animals_info += serialize_animal(animal_obj)
 
     new_html = template.replace("__REPLACE_ANIMALS_INFO__", animals_info)
 
     with open("animals.html", "w") as file:
         file.write(new_html)
+        print("Website was successfully generated to the file animals.html.")
 
 if __name__ == "__main__":
     main()
