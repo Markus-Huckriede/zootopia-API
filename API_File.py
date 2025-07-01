0f3fd42cd963bb6b25e39461853938dcfbf341fa
Markus Huckriede <markushuckriede@999d5eea-c198-41d8-9ebc-c9bdf786196a.fritz.box>
2025-07-01 10:42:47 +0200

    input added, error handling

--- API_File.py
+++ API_File.py
@@ -1,9 +1,13 @@
 import requests
 
-name = input("Enter animal's name: ")
+name = input("Enter a name of an animal: ")
 api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(name)
 response = requests.get(api_url, headers={'X-Api-Key': '0K1fwHLpvK0kCtztNfwR7Q==xpgs5EGeVJ76ua69'})
 if response.status_code == requests.codes.ok:
-    print(response.text)
+    data = response.json()
+    if data:
+        print(data)
+    else:
+        print(f"The animal {name} doesn't exist")
 else:
     print("Error:", response.status_code, response.text)
